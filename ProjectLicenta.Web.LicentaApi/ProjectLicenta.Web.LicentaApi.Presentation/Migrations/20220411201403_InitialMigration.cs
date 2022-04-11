54c0da4a1942e464c33db3c72e8c960822a0e6db
Teodorof Mihaela <mihaela.teodorof.bd@gmail.com>
2022-04-11 23:40:54 +0300

    First Migration

---
+++ ProjectLicenta.Web.LicentaApi/ProjectLicenta.Web.LicentaApi.Presentation/Migrations/20220411201403_InitialMigration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectLicenta.Web.LicentaApi.Presentation.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- ProjectLicenta.Web.LicentaApi/ProjectLicenta.Web.LicentaApi.Presentation/Startup.cs
+++ ProjectLicenta.Web.LicentaApi/ProjectLicenta.Web.LicentaApi.Presentation/Startup.cs
@@ -5,17 +5,39 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Pomelo.EntityFrameworkCore.MySql.Storage;
+using Pomelo.EntityFrameworkCore.MySql.Storage;
+using ServerVersion = Pomelo.EntityFrameworkCore.MySql.Storage.ServerVersion;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using ProjectLicenta.Web.LicentaApi.Infrastructure.Data;
 
 namespace ProjectLicenta.Web.LicentaApi.Presentation
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private IConfiguration Configuration { get; }
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseMySql(Configuration.GetConnectionString("DefaultConnection"),
+                    builderOptions =>
+                    {
+                        builderOptions.MigrationsAssembly("ProjectLicenta.Web.LicentaApi.Presentation")
+                            .ServerVersion(new ServerVersion(new Version(5, 7, 12)))
+                            .CharSet(CharSet.Latin1);
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
